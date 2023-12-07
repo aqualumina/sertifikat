879cba751d137c28129346f7bddb2b3e35a28155
Benedick Yansen Junior <benedickyansenjr@gmail.com>
2023-12-07 21:08:40 +0700

    bebas

---
+++ app/Models/KomikCategoryModel.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class KomikCategoryModel extends Model{
+
+    protected $table = 'komik_category';
+    protected $primaryKey ='komik_category_id';
+}
